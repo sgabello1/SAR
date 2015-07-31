34b06411c6815c740b2e3ce4681f85562d543164
sgabello1 <gabriele.ermacora@yahoo.it>
2015-07-31 15:45:23 +0200

    update

--- src/sar_ros.cpp
+++ src/sar_ros.cpp
@@ -1,7 +1,7 @@
 //______________________________________________________________________________________
 // Taken by  SimplAR 2 - OpenCV Simple Augmented Reality Program with Chessboard
 // SAR demo with chessboard and projector
-// it calulates two homographies Hcb and Hpc. Hpc is calculated by clicking on the picture projected on the wall. Combining the two homograpies 
+// it calulates two homographies Hcb and Hcp. Hcp is calculated by clicking on the picture projected on the wall. Combining the two homograpies 
 // we have sar effect as long as the camera and the projector and on the same plane.
 //
 // Laser Projector
@@ -31,13 +31,14 @@ using namespace cv;
 #define IMAGE_HEIGHT 800
 //The pattern actually has 6 x 5 squares, but has 5 x 4 = 20 'ENCLOSED' corners
 
-vector<Point2f> vClick,vTemplate,vTemplateWarped,vDisplay, vLaser, vLaserWarped;    // stores 4 points that the user clicks(mouse left click) in the main image.
+vector<Point2d> vClick,vCenter,vCenterWarped, vLaser, vLaserWarped;    // stores 4 points that the user clicks(mouse left click) in the main image.
+std::vector<Point2d> vDisplay;
 Mat image,displayWarped,displayWarped_t;
 Mat display;
 Mat  imageDesktop(800,1280, CV_8UC3,Scalar(0,0,255) );
 
-Mat Hpc, Hcb, Htot;
-vector<Point2f> dst;      // Destination Points to transform overlay image  
+Mat Hcp, Hcb,Hcb_laser, Htot;
+vector<Point2d> dst;      // Destination Points to transform overlay image  
 vector<Point2f> corners;  
 cv_bridge::CvImagePtr cv_ptr;
 Mat imageCamera;
@@ -49,38 +50,25 @@ double Xl_image;
 double Yl_image;
 
 // points in laser frame
-float Ax = -0.5;
-float Ay = 0.5;
+double Ax = -0.5;
+double Ay = 0.5;
 
-float Bx = 0.5;
-float By = 0.5;
+double Bx = 0.5;
+double By = 0.5;
 
-float Cx = 0.5;
-float Cy = -0.5;
+double Cx = 0.5;
+double Cy = -0.5;
 
-float Dx = -0.5;
-float Dy = -0.5;
-
-// points in template frame
-float Axt;
-float Ayt;
-
-float Bxt;
-float Byt;
-
-float Cxt;
-float Cyt;
-
-float Dxt;
-float Dyt;
+double Dx = -0.5;
+double Dy = -0.5 ;
 
 // scale factor gammax = (Otx - Btx)/(Olx - Blx) gammay = (Oty - Bty)/(Oly - Bly) 
 // i put the origin Ol = (500,500) in template frame. so i put gammax = gammay = (500-750)/(0-1)=250; Bxt i want it 750
-float gammax, gammay;
+double gammax, gammay;
 
 // translation vector from Ot to Ol tx=ty=500 because i put Ol in the middle of the template frame
-float tx, ty;
-
+double tx = -2.5;
+double ty= 2.5;
 bool token = false;
 bool go = false;
 bool stop;
@@ -91,7 +79,7 @@ Size board_size(CHESSBOARD_WIDTH-1, CHESSBOARD_HEIGHT-1);
 
 bool bTemplate,bTemplate2;
 
-vector<Point2f> projectedPoints(4);      
+vector<Point2d> projectedPoints(4);      
 Mat imageRect;
 
 
@@ -100,21 +88,23 @@ void on_mouse( int e, int x, int y, int d, void *ptr )
     if (e == EVENT_LBUTTONDOWN )
     {
     
-    // STEP 3 - Observe points in image frame and compute Hpc with points in template frame
+    // STEP 3 - Observe points in image frame and compute Hcp with points in the camera frame
     if(vClick.size() < 4 )
         {
  
-            vClick.push_back(Point2f(float(x),float(y))); //points in Ri
+            vClick.push_back(Point2d(double(x),double(y))); //points in Ri
             cout << "points in image frame " << x << " "<< y <<endl;
         }
         else
         {
 
-               
-            Hpc = findHomography(vClick,vTemplate,0);
-            cout << " All right, we have Projector to Camera Homography Hpc " <<endl;
+            cout << " vLaser " << vLaser << " vClick " << vClick <<endl;   
+            Hcp = findHomography(vClick,vLaser,0);
+            cout << " Hcp" << Hcp <<endl;   
 
-            vClick.clear();
+            cout << " All right, we have Projector to Camera Homography Hcp " <<endl;
+
+            //vClick.clear();
             go = true;
           
         }
@@ -131,9 +121,10 @@ void on_mouse( int e, int x, int y, int d, void *ptr )
     if(token ){
     try
     {
-      token = false;
-      cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::BGR8);
-      imageCamera=cv_ptr->image; // fire fly
+      
+    token = false;
+    cv_ptr = cv_bridge::toCvCopy(img, sensor_msgs::image_encodings::BGR8);
+    imageCamera=cv_ptr->image; // fire fly
 
     Mat cpy_image(imageCamera.rows, imageCamera.cols, imageCamera.type());
     Mat neg_image(imageCamera.rows, imageCamera.cols, imageCamera.type());
@@ -159,9 +150,14 @@ void on_mouse( int e, int x, int y, int d, void *ptr )
 
       // perspectiveTransform(displayCorners,projectedPoints, Hcalib); 
       if(dst.size() == 4 && vDisplay.size() == 4){
-        Mat Hcb = findHomography(vDisplay, dst,0);
-      // Htot = findHomography( projectedPoints,dst,0);
 
+        Mat Hcb = findHomography(vDisplay, dst,0);
+        Hcb_laser = findHomography(vLaser, dst,0); //H of the 4 points that I want to project ;)
+        
+        /*if(!go){
+        cout << " dst "<< dst << " \n " << endl;
+        cout << " Hcb_laser "<< Hcb_laser << " \n " << endl;
+       }*/
       blank = Scalar(0);
       neg_image = Scalar(0);                // Image is white when pixel values are zero
       cpy_image = Scalar(0);                // Image is white when pixel values are zero
@@ -173,33 +169,45 @@ void on_mouse( int e, int x, int y, int d, void *ptr )
 
       Mat image2;
       warpPerspective(display, neg_image, Hcb, Size(neg_image.cols, neg_image.rows)); // Transform overlay Image to the position  - [ITEM1]
+
       warpPerspective(blank, cpy_image, Hcb, Size(cpy_image.cols, neg_image.rows));   // Transform a blank overlay image to position  
       bitwise_not(cpy_image, cpy_image);              // Invert the copy paper image from white to black
       bitwise_and(cpy_image, imageCamera, cpy_image);           // Create a "hole" in the Image to create a "clipping" mask - [ITEM2]           
       bitwise_or(cpy_image, neg_image, image2);            // Finally merge both items [ITEM1 & ITEM2]
-
       imshow("Camera 2", image2);
+
       if(go)
       {
 
 
-        //STEP 4 - Transform points in template frame with the Htot= Hcb*Hpc homography
-        Htot = Hcb*Hpc;
-        perspectiveTransform(vTemplate,vTemplateWarped, Htot); 
-        
-        //STEP 5 - transform back points from template frame to laser frame
-        Ax = (vTemplateWarped.at(0).x - tx)/gammax;
-        Bx = (vTemplateWarped.at(1).x - tx)/gammax;
-        Cx = (vTemplateWarped.at(2).x - tx)/gammax;
-        Dx = (vTemplateWarped.at(3).x - tx)/gammax;
+        //STEP 4 - Transform points in template frame with the Htot= Hcb*Hcp homography
+        //Htot = Hcb_laser*Hcp;
 
-        Ay = (ty - vTemplateWarped.at(0).y)/gammay;
-        By = (ty - vTemplateWarped.at(1).y)/gammay;
-        Cy = (ty - vTemplateWarped.at(2).y)/gammay;
-        Dy = (ty - vTemplateWarped.at(3).y)/gammay;
+        perspectiveTransform(dst,vLaserWarped, Hcp); 
+        //perspectiveTransform(vCenter,vCenterWarped, Htot);       
+        
+        //STEP 5 - transform back points from camera frame to laser frame (cam-to-proj)
+
+        Ax = vLaserWarped.at(0).x; //is just A the origin in (0,0)
+        Bx = vLaserWarped.at(1).x;
+        Cx = vLaserWarped.at(2).x;
+        Dx = vLaserWarped.at(3).x;
+
+        Ay = vLaserWarped.at(0).y; //is just A the origin in (0,0)
+        By = vLaserWarped.at(1).y; 
+        Cy = vLaserWarped.at(2).y; 
+        Dy = vLaserWarped.at(3).y;
+
+        cout << "STEP 5 - transform back points from template frame to laser frame" << endl;
+
+        cout << "\n vLaserWarped " << vLaserWarped << "\n vLaser " <<  vLaser << endl;
+        
+        cout << " Ax " << Ax << " Ay " << Ay << endl;
+        cout << " Bx " << Bx << " By " << By << endl;
+        cout << " Cx " << Cx << " Cy " << Cy << endl;
+        cout << " Dx " << Dx << " Dy " << Dy << endl;
 
         msg.data.clear();
-
         msg.data.push_back(2); // size of the vector
         msg.data.push_back(Ax); //0
         msg.data.push_back(Ay); //1
@@ -208,17 +216,25 @@ void on_mouse( int e, int x, int y, int d, void *ptr )
         msg.data.push_back(Cx); //4
         msg.data.push_back(Cy); //5
         msg.data.push_back(Dx); //6
-        msg.data.push_back(Dy); //7*/
-
+        msg.data.push_back(Dy); //7
+        */
       }
 
-      dst.clear();
+        dst.clear();
+
       }
     }
 
     setMouseCallback("Camera",on_mouse, NULL );
 
+    //draw points in the template
+    /*circle(imageCamera, vTemplate[0], 7, Scalar(0,0,255),-1); //BGR
+    circle(imageCamera, vTemplate[1], 7, Scalar(0,0,255),-1);
+    circle(imageCamera, vTemplate[2], 7, Scalar(0,0,255),-1);
+    circle(imageCamera, vTemplate[3], 7, Scalar(0,0,255),-1);
+*/
     imshow("Camera", imageCamera);
+
     }
     catch (cv_bridge::Exception& e)
     {
@@ -238,16 +254,20 @@ int main ( int argc, char **argv )
 
   token = true;
   stop = false;
-  gammax = gammay = 250;
+  gammax = 106.666667;
+  gammay = 80;
 
   display = imread("/home/sgabello/catkin_ws/src/spatially_ar/src/cb.png");
   //imageDesktop = imread("/home/sgabello/catkin_ws/src/spatially_ar/src/cb.png", CV_LOAD_IMAGE_COLOR);
 
   // corners of the image to be projected
-  vDisplay.push_back(Point2f(float(0),float(0)));
-  vDisplay.push_back(Point2f(float(display.cols),float(0)));
-  vDisplay.push_back(Point2f(float(display.cols),float(display.rows)));
-  vDisplay.push_back(Point2f(float(0),float(display.rows)));
+  vDisplay.push_back(Point2d(float(0),double(0)));
+  vDisplay.push_back(Point2d(double(display.cols),double(0)));
+  vDisplay.push_back(Point2d(double(display.cols),double(display.rows)));
+  vDisplay.push_back(Point2d(double(0),double(display.rows)));
+
+  //center
+  vCenter.push_back(Point2d(double(0),double(0))); 
 
   cout << vDisplay << "  vDisplay " << endl;
 
@@ -273,22 +293,12 @@ int main ( int argc, char **argv )
   msg.data.push_back(Dy); //7
 
 
-  // STEP 2 - transform points in Template Reference (proj-to-template)
-  Axt = Ax*gammax + tx;
-  Bxt = Bx*gammax + tx;
-  Cxt = Cx*gammax + tx;
-  Dxt = Dx*gammax + tx;
+  //laser coordinates
+  vLaser.push_back(Point2d(Ax,Ay));
+  vLaser.push_back(Point2d(Bx,By));
+  vLaser.push_back(Point2d(Cx,Cy));
+  vLaser.push_back(Point2d(Dx,Dy));
 
-  Ayt = ty - gammay*Ay;
-  Byt = ty - gammay*By;
-  Cyt = ty - gammay*Cy;
-  Dyt = ty - gammay*Dy;
-
-  // put points in the vector vTemplate for next step
-  vTemplate.push_back(Point2f(Axt,Ayt));
-  vTemplate.push_back(Point2f(Bxt,Byt));
-  vTemplate.push_back(Point2f(Cxt,Cyt));
-  vTemplate.push_back(Point2f(Dxt,Dyt));
 
   while((char) key != 'q')
   {
